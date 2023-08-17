3ba8a3a7003d5f580ab7633f235b4b815fbba28a
s-a-i-m-o-n <harms.simon759@mail.kyutech.jp>
2023-08-17 09:47:39 +0900

    Changed joint limits in build config, minor changes

--- tbotsim/script_build config.py
+++ tbotsim/script_build config.py
@@ -11,29 +11,29 @@ holds = []
 
 for position in hold_positions:
     holds.append(TbHold(T_local = position, 
-                        geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Wall_Dock Hold.STL'))],
+                        geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Wall_Dock Hold.stl'))],
                         hoverpoint = TbHoverPoint(T_local = [0,0,0.039]),
                         grippoint = TbGripPoint(T_local = [0,0,0.029])))
 
 # create wall
 wall = TbWall(holds = holds, 
-              geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Wall_Board.STL'))])
+              geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Wall_Board.stl'))])
 
 # create arm
 links = []
 links.append(TbRevoluteLink(q0=0, alpha=-pi/2, a=0, d=0.0435, qlim=[-pi, pi], 
-                            geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform_Arm_Joint 1.STL'))])
+                            geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform_Arm_Joint 1.stl'))])
                             )
 links.append(TbPrismaticLink(phi=0, alpha=-pi/2, a=0, q0=0.381, qlim=[0.375,0.961], 
-                             geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform_Arm_Joint 2.STL'))])
+                             geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform_Arm_Joint 2.stl'))])
                              )
 
 links.append(TbPrismaticLink(phi=0, alpha=0, a=0, q0=0.198, qlim=[0.074,0.198], 
-                             geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform_Arm_Joint 3.STL'))]))
+                             geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform_Arm_Joint 3.stl'))]))
 
 arm = TbRPPArm(T_local = [0,0,0.167],
                links = links,
-               geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform_Arm_Base.STL'))])
+               geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform_Arm_Base.stl'))])
 
 # create platform
 anchorpoint_positions = np.array([[0.285,0,0.008],[0.220,-0.130,0.008],[-0.220,-0.120,0.008],[-0.220,0.120,0.008],
@@ -48,7 +48,7 @@ platform = TbPlatform(T_local = [0.423,1.020,0.068,0,0,90],
                       arm = arm , 
                       anchorpoints = anchorpoints,
                       depthsensor = TbDepthsensor(T_local = [-0.27645,0,0.14625,0,180,0]),
-                      geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform.STL'))])
+                      geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Platform.stl'))])
 
 # create grippers
 grippers = []
@@ -59,13 +59,13 @@ for i in range(5):
                               anchorpoint = TbAnchorPoint(T_local = [0,0,0.0815]),
                               grippoint = TbGripPoint(T_local = [0,0,0.029]),
                               marker = TbMarker(T_local= [0,-0.011,0.089,90,0,0]),
-                              geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Dock Gripper.STL'))]))
+                              geometries = [TbTrianglemesh(filename=os.path.join(absolute_path, 'stl/Tetherbot_Dock Gripper.stl'))]))
     
 # create tethers
 tethers = []
 
 for i in range(10):
-    tethers.append(TbTether(geometries = [TbTethergeometry(radius = 2)],
+    tethers.append(TbTether(geometries = [TbTethergeometry(radius = 0.005)],
                             f_min = 0,
                             f_max = 100))
 
@@ -81,12 +81,14 @@ tbot.place_all([0,1,2,3,4])
 tbot.save(os.path.join(absolute_path, 'pickle/tetherbot.pkl'), overwrite = True)
 
 # create urdf files
-tb2urdf(tbot, prefix = '', packagename = 'ros2_tetherbot', filepath = os.path.join(absolute_path, 'urdf'))
+#tb2urdf(tbot, prefix = '', packagename = 'ros2_tetherbot', filepath = os.path.join(absolute_path, 'urdf'))
 
-vi = TetherbotVisualizer(tbot)
-vi.run()
+#vi = TetherbotVisualizer(tbot)
+#vi.run()
 
+# create light pickle file
 tbot.remove_all_geometries()
-#tbot.save(os.path.join(absolute_path, 'pickle/tetherbot_light.pkl'), overwrite = True)
+tbot.save(os.path.join(absolute_path, 'pickle/tetherbot_light.pkl'), overwrite = True)
+
 """ TbTetherbotplot(tbot)
 show() """
