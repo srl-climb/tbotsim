d00255775d012728f4bf60a9f84cb1b8119ac0d7
s-a-i-m-o-n <harms.simon759@mail.kyutech.jp>
2023-08-29 10:43:04 +0900

    Updated config and run local path planning simulation

--- tbotsim/script_command player.py
+++ tbotsim/script_command player.py
@@ -1,12 +1,12 @@
 from tbotlib import CommandList, TetherbotVisualizer, TbTetherbot
 
-commands_path = 'C:/Users/ngkla/Desktop/Git/tbotsim/tbotsim/commands.yaml'
+commands_path = 'C:/Users/ngkla/Desktop/Git/tbotsim/tbotsim/commands_2_2.yaml'
 tbot_path = 'C:/Users/ngkla/Desktop/Git/tbotsim/tbotsim/pickle/tetherbot.pkl'
 
 commands = CommandList.load(commands_path)
 tbot: TbTetherbot = TbTetherbot.load(tbot_path)
 
-
+print(tbot.W)
 """ vi = TetherbotVisualizer(tbot)
 done = True
 while commands and vi.opened:
