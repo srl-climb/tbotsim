c22a305babc725ee90ebe573a3da3f0e5a64e378
s-a-i-m-o-n <harms.simon759@mail.kyutech.jp>
2023-09-07 08:57:57 +0900

    Minor changes

--- tbotsim/script_command player.py
+++ tbotsim/script_command player.py
@@ -7,16 +7,16 @@ commands = CommandList.load(commands_path)
 tbot: TbTetherbot = TbTetherbot.load(tbot_path)
 
 print(tbot.W)
-""" vi = TetherbotVisualizer(tbot)
+vi = TetherbotVisualizer(tbot)
 done = True
 while commands and vi.opened:
     vi.update()
     if done:
         command = commands.pop(0)
         command.print()
-    done = command.do(tetherbot=tbot, step = 100)
+    done = command.do(tetherbot=tbot, step = 200)
     print(tbot.stability())
-vi.run() """
+vi.run()
 
 for command in commands: 
     command.print()
